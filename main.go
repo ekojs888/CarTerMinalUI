b4eba5edab5b22561fb124cb9ec12de8ceae5fed
eko johari s <ekojs999@gmail.com>
2022-02-21 01:27:28 +0800

    update

--- main.go
+++ main.go
@@ -57,57 +57,63 @@ func readFlasdisk(ui ui) {
 }
 
 func main() {
-	db := Databases{}
+	// db := Databases{}
 	ui := ui{}
 	ui.NewBarChart()
 	ui.NewTable()
 	ui.NewListMusic()
 
-	db.ConSqlLite()
-	db.Init()
-	db.Inserts(
-		&[]TblData{
-			{
-				Name:     "Bensin",
-				Value:    20,
-				ValueMax: 40,
-				Satuan:   "Liter",
-			},
-			{
-				Name:     "Mesin",
-				Value:    7000,
-				ValueMax: 10000,
-				Satuan:   "Rpm",
-			},
-			{
-				Name:     "Aki",
-				Value:    12,
-				ValueMax: 13,
-				Satuan:   "Volt",
-			},
-		},
-	)
+	// db.ConSqlLite()
 	// db.Init()
-	db.UpdateByName("Bensin", 15)
+	// db.Inserts(
+	// 	&[]TblData{
+	// 		{
+	// 			Name:     "Bensin",
+	// 			Value:    20,
+	// 			ValueMax: 40,
+	// 			Satuan:   "Liter",
+	// 		},
+	// 		{
+	// 			Name:     "Mesin",
+	// 			Value:    7000,
+	// 			ValueMax: 10000,
+	// 			Satuan:   "Rpm",
+	// 		},
+	// 		{
+	// 			Name:     "Aki",
+	// 			Value:    12,
+	// 			ValueMax: 13,
+	// 			Satuan:   "Volt",
+	// 		},
+	// 	},
+	// )
+	// // db.Init()
+	// db.UpdateByName("Bensin", 15)
 
 	go func() {
 		var a float64 = 0
 		for {
-			time.Sleep(30 * time.Millisecond)
-			db.UpdateByName("Bensin", a)
-			db.UpdateByName("Mesin", a/40*10000)
-			db.UpdateByName("Aki", a/40*13)
+			time.Sleep(500 * time.Millisecond)
+			// db.UpdateByName("Bensin", a)
+			// db.UpdateByName("Mesin", a/40*10000)
+			// db.UpdateByName("Aki", a/40*13)
 			if a >= 40 {
 				a = 0
 			} else {
 				a++
 			}
 
-			dt := db.List()
-			for _, val := range dt {
-				ui.SetBarValue(val.Name, int((val.Value/val.ValueMax)*100))
-				ui.SetValTable(int(val.ID)-1, strconv.Itoa(int(val.Value)))
-			}
+			// dt := db.List()
+			// for _, val := range dt {
+			// 	ui.SetBarValue(val.Name, int((val.Value/val.ValueMax)*100))
+			// 	ui.SetValTable(int(val.ID)-1, strconv.Itoa(int(val.Value)))
+			// }
+
+			ui.SetBarValue("Bensin", int(a))
+			ui.SetValTable(0, strconv.Itoa(int((a/40)*100)))
+
+			ui.SetBarValue("Suhu", int(a))
+			ui.SetValTable(1, strconv.Itoa(int((a/40)*100)))
 
 			readFlasdisk(ui)
 
@@ -124,11 +130,17 @@ func main() {
 		tcell.ColorWhite,
 		tcell.ColorOrange,
 	}
-	dt := db.List()
-	for key, val := range dt {
-		ui.AddBarItem(val.Name, int((val.Value/val.ValueMax)*100), color[key])
-		ui.AddTableItem(val.Name, strconv.Itoa(int(val.Value)), val.Satuan)
-	}
+	// dt := db.List()
+	// for key, val := range dt {
+	// 	ui.AddBarItem(val.Name, int((val.Value/val.ValueMax)*100), color[key])
+	// 	ui.AddTableItem(val.Name, strconv.Itoa(int(val.Value)), val.Satuan)
+	// }
+
+	ui.AddBarItem("Bensin", 100, color[0])
+	ui.AddTableItem("Bensin", "100", "Liter")
+
+	ui.AddBarItem("Suhu", 36, color[1])
+	ui.AddTableItem("Bensin", "30", "°C")
 
 	ui.NewApp()
 	ui.Run()
